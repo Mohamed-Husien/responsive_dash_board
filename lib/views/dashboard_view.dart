5bbb27b3e900a3b43ae62b236286c7a988b6bd4b
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-27 10:18:13 +0200

    add dash board desktop layout

--- lib/views/dashboard_view.dart
+++ lib/views/dashboard_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board_app/widgets/adaptive_layout.dart';
+import 'package:responsive_dash_board_app/widgets/dashboard_desktop_layout.dart';
 
 class DashBoardView extends StatelessWidget {
   const DashBoardView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: AdaptiveLayout(
+          mobileLayout: (context) => const SizedBox(),
+          tabletLayout: (context) => const SizedBox(),
+          desktopLayout: (context) => const DashboardDesktopLayout()),
+    );
   }
 }
