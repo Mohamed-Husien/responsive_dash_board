dc966fde0ed47d492454c4bfda5281a3ee9b61da
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-28 09:23:16 +0200

    finish user info list tile

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board_app/utils/app_images.dart';
 import 'package:responsive_dash_board_app/widgets/usr_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -10,7 +11,11 @@ class CustomDrawer extends StatelessWidget {
       color: Colors.white,
       child: const Column(
         children: [
-          UsrInfoListTile(),
+          UsrInfoListTile(
+            image: Assets.imagesAvatar3,
+            title: 'Mohamed Hussien',
+            subTitle: "mohamed@gmail.com",
+          ),
         ],
       ),
     );
--- lib/widgets/usr_info_list_tile.dart
+++ lib/widgets/usr_info_list_tile.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:responsive_dash_board_app/utils/app_styles.dart';
 
 class UsrInfoListTile extends StatelessWidget {
-  const UsrInfoListTile({super.key});
-
+  const UsrInfoListTile(
+      {super.key,
+      required this.title,
+      required this.subTitle,
+      required this.image});
+  final String title, subTitle, image;
   @override
   Widget build(BuildContext context) {
-    return const ListTile();
+    return Card(
+      color: const Color(0xFFFAFAFA),
+      elevation: 0,
+      child: ListTile(
+        leading: SvgPicture.asset(image),
+        title: Text(
+          title,
+          style: AppStyle.styleSemiBold16,
+        ),
+        subtitle: Text(
+          subTitle,
+          style: AppStyle.styleRegular12,
+        ),
+      ),
+    );
   }
 }
