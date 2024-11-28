3c5e8ef4c44e571e031c6ba60efbba016cdd7044
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-28 11:42:54 +0200

    fixe issus in height of drawer change and finish drawer part

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board_app/models/drawer_item_model.dart';
 import 'package:responsive_dash_board_app/utils/app_images.dart';
+import 'package:responsive_dash_board_app/widgets/active_and_inactive_item.dart';
 
 import 'package:responsive_dash_board_app/widgets/drawer_items_list_view.dart';
 import 'package:responsive_dash_board_app/widgets/usr_info_list_tile.dart';
@@ -11,17 +13,44 @@ class CustomDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       color: Colors.white,
-      child: const Column(
-        children: [
-          UsrInfoListTile(
-            image: Assets.imagesAvatar3,
-            title: 'Mohamed Hussien',
-            subTitle: "mohamed@gmail.com",
+      child: const CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: UsrInfoListTile(
+              image: Assets.imagesAvatar3,
+              title: 'Mohamed Hussien',
+              subTitle: "mohamed@gmail.com",
+            ),
           ),
-          SizedBox(
-            height: 8,
+          SliverToBoxAdapter(
+            child: SizedBox(
+              height: 8,
+            ),
           ),
-          DrawerItemsListView()
+          DrawerItemsListView(),
+          SliverFillRemaining(
+            hasScrollBody: false,
+            child: Column(
+              children: [
+                Expanded(child: SizedBox()),
+                InActiveItem(
+                  drawerItemModel: DrawerItemModel(
+                    title: "Setting system",
+                    image: Assets.imagesSettings,
+                  ),
+                ),
+                InActiveItem(
+                  drawerItemModel: DrawerItemModel(
+                    title: "Logout account",
+                    image: Assets.imagesLogout,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
--- lib/widgets/drawer_items_list_view.dart
+++ lib/widgets/drawer_items_list_view.dart
@@ -26,9 +26,7 @@ class _DrawerItemsListViewState extends State<DrawerItemsListView> {
   int activeItem = 0;
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      shrinkWrap: true,
-      physics: const NeverScrollableScrollPhysics(),
+    return SliverList.builder(
       itemCount: items.length,
       itemBuilder: (context, index) {
         return GestureDetector(
