978d477c6a414204525371ec32ef0764a7b48e69
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-28 08:49:20 +0200

    create custom drawer

--- lib/widgets/dashboard_desktop_layout.dart
+++ lib/widgets/dashboard_desktop_layout.dart
@@ -5,6 +5,8 @@ class DashboardDesktopLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Row(
+      children: [],
+    );
   }
 }
