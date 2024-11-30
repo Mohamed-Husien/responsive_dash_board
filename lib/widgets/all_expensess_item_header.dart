195af8ecf606239186b0d5581778a823304557ed
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-30 10:24:08 +0200

    create all expensess item model

--- lib/widgets/all_expensess_item_header.dart
+++ lib/widgets/all_expensess_item_header.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class AllExpensessItemHeader extends StatelessWidget {
-  const AllExpensessItemHeader({super.key});
-
+  const AllExpensessItemHeader({super.key, required this.image});
+  final String image;
   @override
   Widget build(BuildContext context) {
-    return const Row(
-      children: [],
+    return Row(
+      children: [
+        Container(
+          width: 60,
+          height: 60,
+          decoration: const ShapeDecoration(
+            color: Color(0xFFFAFAFA),
+            shape: OvalBorder(),
+          ),
+          child: SvgPicture.asset(image),
+        ),
+        const Icon(
+          Icons.arrow_forward_ios_rounded,
+          color: Color(0xff064061),
+        ),
+      ],
     );
   }
 }
