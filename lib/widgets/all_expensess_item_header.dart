b4a2ff707d1ba2036140a61dbf344ec250c35727
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-30 10:01:33 +0200

    create all expensess item header

---
+++ lib/widgets/all_expensess_item_header.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AllExpensessItemHeader extends StatelessWidget {
+  const AllExpensessItemHeader({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(
+      children: [],
+    );
+  }
+}
