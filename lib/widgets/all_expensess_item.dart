d95d4cd5221346720255820b72d1fff9399f36ec
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-11-30 13:35:50 +0200

    add animation when switch between expensess item

--- lib/widgets/all_expensess_item.dart
+++ lib/widgets/all_expensess_item.dart
@@ -1,17 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board_app/models/all_expensess_item_model.dart';
-
 import 'package:responsive_dash_board_app/widgets/inactive_and_active_all_expensess_item.dart';
 
 class AllExpensessItem extends StatelessWidget {
-  const AllExpensessItem(
-      {super.key, required this.itemModel, required this.isSelected});
+  const AllExpensessItem({
+    super.key,
+    required this.itemModel,
+    required this.isSelected,
+  });
+
   final AllExpensessItemModel itemModel;
   final bool isSelected;
+
   @override
   Widget build(BuildContext context) {
-    return isSelected
-        ? ActiveAllExpensessItem(itemModel: itemModel)
-        : InActiveAllExpensessItem(itemModel: itemModel);
+    return AnimatedCrossFade(
+      duration: const Duration(milliseconds: 300),
+      firstChild: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        color: Colors.grey[300],
+        child: InActiveAllExpensessItem(itemModel: itemModel),
+      ),
+      secondChild: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        color: Colors.blue[300],
+        child: ActiveAllExpensessItem(itemModel: itemModel),
+      ),
+      crossFadeState:
+          isSelected ? CrossFadeState.showSecond : CrossFadeState.showFirst,
+    );
   }
 }
