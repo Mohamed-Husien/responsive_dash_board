2f72ad5891d803c8dc8e1106ebb576bbe1bfb4e6
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-04 09:18:56 +0200

    my card part tow

--- lib/widgets/my_card.dart
+++ lib/widgets/my_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:responsive_dash_board_app/utils/app_images.dart';
+import 'package:responsive_dash_board_app/utils/app_styles.dart';
 
 class MyCard extends StatelessWidget {
   const MyCard({super.key});
@@ -17,6 +19,25 @@ class MyCard extends StatelessWidget {
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(12),
           ),
+        ),
+        child: Column(
+          children: [
+            ListTile(
+              contentPadding:
+                  const EdgeInsets.only(left: 31, right: 42, top: 16),
+              title: Text(
+                "Name card",
+                style: AppStyle.styleRegular16.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: const Text(
+                "Mo Hussien",
+                style: AppStyle.styleMedium20,
+              ),
+              trailing: SvgPicture.asset(Assets.imagesGallery),
+            ),
+          ],
         ),
       ),
     );
