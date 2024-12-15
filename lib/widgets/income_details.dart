f8a93d5093a8f6a8ee0f6841f19f913ba4c1e36f
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-15 11:16:29 +0200

    create item details and item details model

--- lib/widgets/income_details.dart
+++ lib/widgets/income_details.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board_app/models/item_details_model.dart';
+import 'package:responsive_dash_board_app/utils/app_styles.dart';
 
 class IncomeDetails extends StatelessWidget {
   const IncomeDetails({super.key});
---
+++ lib/widgets/item_details.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board_app/models/item_details_model.dart';
+import 'package:responsive_dash_board_app/utils/app_styles.dart';
+
+class ItemeDetails extends StatelessWidget {
+  const ItemeDetails({super.key, required this.itemDetailsModel});
+  final ItemDetailsModel itemDetailsModel;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Container(
+        width: 12,
+        height: 12,
+        decoration: ShapeDecoration(
+          color: itemDetailsModel.color,
+          shape: const OvalBorder(),
+        ),
+      ),
+      title: Text(
+        itemDetailsModel.title,
+        style: AppStyle.styleRegular16,
+      ),
+      trailing: Text(
+        itemDetailsModel.value,
+        style: AppStyle.styleMedium16,
+      ),
+    );
+  }
+}
