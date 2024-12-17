2494663d74a146d483c15c904eac7d014c4fe109
Mohamed Hussien <mohamedelsayedhussien@gmail.com>
2024-12-17 09:58:31 +0200

    create size config class

--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board_app/widgets/custom_background_container.dart';
 import 'package:responsive_dash_board_app/widgets/custom_section_header.dart';
-import 'package:responsive_dash_board_app/widgets/income_chart.dart';
-import 'package:responsive_dash_board_app/widgets/income_details.dart';
+
 import 'package:responsive_dash_board_app/widgets/income_section_body.dart';
 
 class IncomeSection extends StatelessWidget {
